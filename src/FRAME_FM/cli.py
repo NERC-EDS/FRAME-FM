0e1e4a858c21ec5024d50a95d2ed0377518a5c5c
tom g <thogar@noc.ac.uk>
2026-02-02 21:23:28 +0000

    working Click to hydra setup

--- src/FRAME_FM/cli.py
+++ src/FRAME_FM/cli.py
@@ -3,20 +3,23 @@ import click
 from pathlib import Path
 #from ./training/train.py import main as train_main
 
-@click.command()
+@click.command(context_settings=dict(
+    ignore_unknown_options=True,
+))
 @click.option('--config', 
               prompt='Please enter the config file/folder path', 
               help='The config file or folder path to use for training.',
               type=click.Path(exists=True, file_okay=True, dir_okay=True, path_type=Path)
               )
+@click.argument("args", nargs=-1, type=click.UNPROCESSED)
 @click.pass_context
-def wrapper(ctx, config):    
+def wrapper(ctx, config, args):    
     """Simple program that greets NAME."""
     click.echo(f"Training with config: {config}")
-    hydra_args = ctx.args
+    hydra_args = args
     from hydra import initialize_config_dir, compose
     with initialize_config_dir(config_dir=f"{config}"):   
-        cfg = compose(config_name=config, overrides=hydra_args)
+        cfg = compose(config_name="config.yml", overrides=hydra_args)
         print(cfg)
         #train_main()
 
