a3e4235a9246a89d503c7e1bd47c124e3c61eac2
Colin Sauze <colin.sauze@noc.ac.uk>
2026-03-10 18:06:47 +0000

    putting cosmos UK stuff in datasets not dataloaders Fixing paths in cosmos UK tests

--- tests/datasets/cosmos-uk/test_cosmos-uk_qc.py
+++ tests/datasets/cosmos-uk/test_cosmos-uk_qc.py
@@ -1,6 +1,5 @@
 import pytest
-import csv_loader
-#tests to write
+import FRAME_FM.datasets.cosmosuk_dataset
 
 import math
 import numpy as np
@@ -14,51 +13,43 @@ import numpy as np
 # test QC masks
 
 def test_qc_mask_all_flags():
-    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/", 0b11111111111, [])
-    assert ds['LWIN'].values[0] == 339.4
-    # annoyingly python won't let us assert against a list of nans properly
-    # so we've got to do it one by one
-    for i in range(1,9):
-        assert math.isnan(ds['LWIN'].values[i])
+    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/datasets/cosmos-uk/", 0b11111111111, [])
+    assert np.array_equal(ds['LWIN'].values,[339.4, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],equal_nan=True)
 
 def test_qc_mask_missing():
-    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/", 0b0000000001, [])
+    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/datasets/cosmos-uk/", 0b0000000001, [])
     assert np.array_equal(ds['LWIN'].values,[339.4, math.nan, 316.6,  316.5, 313.0, 316.8, 323.6, math.nan, math.nan],equal_nan=True)
 
 def test_qc_mask_no_flags():
-    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/", 0b0000000000, [])
+    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/datasets/cosmos-uk/", 0b0000000000, [])
     assert np.array_equal(ds['LWIN'].values,[339.4, 329.4, 316.6,  316.5, 313.0, 316.8, 323.6, 336.2, 324.0],equal_nan=True)
     
 # test flags
 def test_flags_none():
-    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/", 0b0000000000, [])
+    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/datasets/cosmos-uk/", 0b0000000000, [])
     assert np.array_equal(ds['LWIN'].values, [339.4, 329.4, 316.6,  316.5, 313.0, 316.8, 323.6, 336.2, 324.0],equal_nan=True)
 
 def test_flags_all():
-    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/", 0b0000000000, ["I", "M", "E", "U"])
+    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/datasets/cosmos-uk/", 0b0000000000, ["I", "M", "E", "U"])
     assert np.array_equal(ds['LWIN'].values, [math.nan, math.nan, math.nan, math.nan, 313.0, 316.8, 323.6, 336.2, 324.0],equal_nan=True)
 
-# test flags
-
 def test_flags_u():
-    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/", 0b0000000000, ["U"])
+    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/datasets/cosmos-uk/", 0b0000000000, ["U"])
     assert np.array_equal(ds['LWIN'].values,[math.nan, 329.4, 316.6,  316.5, 313.0, 316.8, 323.6, 336.2, 324.0],equal_nan=True)
 
-
 def test_flags_i():
-    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/", 0b0000000000, ["I"])
+    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/datasets/cosmos-uk/", 0b0000000000, ["I"])
     assert np.array_equal(ds['LWIN'].values,[339.4, math.nan, 316.6,  316.5, 313.0, 316.8, 323.6, 336.2, 324.0],equal_nan=True)
 
 def test_flags_e():
-    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/", 0b0000000000, ["E"])
+    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/datasets/cosmos-uk/", 0b0000000000, ["E"])
     assert np.array_equal(ds['LWIN'].values,[339.4, 329.4, math.nan, 316.5, 313.0, 316.8, 323.6, 336.2, 324.0],equal_nan=True)
 
 def test_flags_m():
-    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/", 0b0000000000, ["M"])
+    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/datasets/cosmos-uk/", 0b0000000000, ["M"])
     assert np.array_equal(ds['LWIN'].values,[339.4, 329.4, 316.6, math.nan, 313.0, 316.8, 323.6, 336.2, 324.0],equal_nan=True)
 
 def test_flags_invalid():
     # specify a flag we don't use, all data should be present
-    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/", 0b0000000000, ["A"])
+    ds = csv_loader.csv_to_xarray("FRAME-FM/tests/datasets/cosmos-uk/", 0b0000000000, ["A"])
     assert np.array_equal(ds['LWIN'].values,[339.4, 329.4, 316.6, 316.5, 313.0, 316.8, 323.6, 336.2, 324.0],equal_nan=True)
-
