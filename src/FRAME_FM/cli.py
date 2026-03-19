426df89d9c3ca93965bd27b91043b8183a52d072
james Clare <jamclar@noc.ac.uk>
2026-03-19 16:36:28 +0000

    Add remainder of command line options.

--- src/FRAME_FM/cli.py
+++ src/FRAME_FM/cli.py
@@ -2,12 +2,14 @@
 
 
 from collections import defaultdict
-from rich.console import Console
-from rich.panel import Panel
-from rich.syntax import Syntax
+from pathlib import Path
 
 import click
-from pathlib import Path
+import yaml
+from rich.console import Console
+from rich.panel import Panel
+from rich.pretty import Pretty
+from rich.syntax import Syntax
 
 
 console = Console()
@@ -26,15 +28,57 @@ def show_config_files() -> None:
 
 def display_contents_of_config_file(config_file: str) -> None:
     """Display the contents of a config file."""
-
     files = list(Path("configs").rglob(config_file))
     if not files:
         click.secho(f"No matching config found for file: {config_file}.", fg="red")
+        return
     
     for file in files:
         console.print(f"File: {file}")
         console.print(Syntax(file.open().read(), "yaml", theme="monokai", line_numbers=True))
 
+def view_hydra_defaults() -> None:
+    """Display the Hydra default values from the config."""
+    with Path("configs/config.yaml").open() as f:
+        contents = yaml.safe_load(f.read())
+
+    
+
+    if (defaults := contents.get("defaults")) is not None:
+        console.print(
+    Panel(Pretty(defaults), title="Hydra Defaults", expand=False)
+)
+    else:
+        click.secho("Unable to find Hydra config file: configs/config.yaml", fg="red")
+        
+
+
+def edit_config_file(config_file: str, key_value_pairs: str) -> None:
+    """Edit the config file, raising errors if the format is incorrect, or the key is missing."""
+    with Path(config_file).open() as file:
+        file = yaml.safe_load(file.read())
+
+    for pair in key_value_pairs.split(","):
+        # Verify that the format is correct.
+        if ":" not in pair or len(pair.split(":")) != 2:
+            raise click.BadParameter("Expected format -> key:value")
+        
+
+        key, value = pair.split(":")
+        if key not in file:
+            raise click.BadParameter(f"Key '{key}' not found in config.")
+        
+        if isinstance(file[key], float):
+            file[key] = float(value)
+        elif isinstance(file[key], int):
+            file[key] = int(value)
+        else:
+            file[key] = value
+
+    with open(config_file, mode="w") as edited_file:
+        edited_file.write(yaml.dump(file))
+
+        
 
 @click.group()
 def app():
@@ -56,21 +100,46 @@ def app():
     """
     pass
 
-@app.command()
+
+@click.command()
 def train():
     """Launch a model training run."""
     click.echo("Training command invoked.") # This is a placeholder for the time being.
 
 
 
-@app.command()
-@click.option("--list", "list_all", is_flag=True)
-@click.option("--display", "display_config", help="Pass either the filename in the form of 'file.yaml', or 'folder/file.yaml'.")
-def config(list_all, display_config):
-    """Launch configuration entrypoint."""
-    
-    if list_all:
-        show_config_files()
+@click.group()
+def config():
+    """Configuration entrypoint."""
+    pass
 
-    if display_config:
-        display_contents_of_config_file(display_config)
+
+@config.command("list")
+def list_configs():
+    """List available config files."""
+    show_config_files()
+
+
+@config.command("display")
+@click.argument("config_file", type=click.Path(dir_okay=False))
+def display(config_file):
+    """Display the contents of a config file."""
+    display_contents_of_config_file(config_file)
+
+
+@config.command("view-defaults")
+def view_defaults():
+    """Show Hydra defaults."""
+    view_hydra_defaults()
+
+
+@config.command("edit")
+@click.argument("config_file", type=click.Path(exists=True, dir_okay=False))
+@click.argument("key_value_pairs")
+def edit(config_file, key_value_pairs):
+    """Edit values within a specified config file."""
+    edit_config_file(config_file=config_file, key_value_pairs=key_value_pairs)
+
+
+app.add_command(config)
+app.add_command(train)
