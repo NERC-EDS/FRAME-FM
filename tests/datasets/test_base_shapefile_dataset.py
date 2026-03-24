5c74ce08506cb3c998d4c9d757d4437d42d23f09
Colin Sauze <colin.sauze@noc.ac.uk>
2026-03-24 17:59:34 +0000

    removing redudant Shapefiletoxarray.py ruff formatting test

--- tests/datasets/test_base_shapefile_dataset.py
+++ tests/datasets/test_base_shapefile_dataset.py
@@ -3,12 +3,24 @@ import pytest
 
 from FRAME_FM.datasets.base_shapefile_dataset import BaseShapefileDataset
 
-def test_dimensions():
-    # Specify the path to the config file. 
-    cfg_path = '/home/users/colinsau/FRAME-FM/configs/data/config_FRAME_shpfiles.yaml'
+
+def test_dimensions_and_attributes():
+    """
+    Test the dimensions and attributes of the dataset are as expected.
+    """
+    # Specify the path to the config file.
+    cfg_path = "/home/users/colinsau/FRAME-FM/configs/data/config_FRAME_shpfiles.yaml"
 
     # Set up the class and build the dataset.
-    r = BaseShapefileDataset(data_uri = cfg_path)
-    
-    assert r.dataset_out.sizes['x'] == 604
-    assert r.dataset_out.sizes['y'] == 1212
+    r = BaseShapefileDataset(data_uri=cfg_path)
+
+    assert r.dataset_out.sizes["x"] == 604, (
+        f"Expected width of dataset is 604, got {r.dataset_out.sizes['x']}"
+    )
+    assert r.dataset_out.sizes["y"] == 1212, (
+        f"Expected width of dataset is 1212, got {r.dataset_out.sizes['y']}"
+    )
+    assert r.resolution == 1000, (
+        f"Expected resolution is 1000, got {r.dataset_out.resolution}"
+    )
+    assert r.target_crs == "EPSG:27700", f"Expect CRS is ESPG:27700, got {r.target_crs}"
